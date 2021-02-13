5a939b5e75cdc1a6972aa3eabb02673234a5e97f
Brandon Maroni <63432771+Bmmaroni@users.noreply.github.com>
2021-02-13 13:03:58 -0700

    finished POST and PUT endpoints

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -35,3 +35,45 @@ router.get("/api/posts/:id", (req, res) => {
         }) 
        })
 })
+
+router.post("/api/posts", (req, res) => {
+  if (!req.body.title || !req.body.contents) {
+    return res.status(400).json({
+      message: "Please provide title and contents for the post"
+    })
+  }
+  posts.insert(req.body)
+      .then((post) => {
+          res.status(201).json(post)
+      })
+      .catch((err) => {
+        console.log(err)
+        res.status(500).json({
+         message: "There was an error while saving the post to the database"
+        }) 
+       })
+})
+
+router.put("/api/posts/:id", (req, res) => {
+  if (!req.body.title || !req.body.contents) {
+    return res.status(400).json({
+      message: "Please provide title and contents for the post"
+    })
+  }
+  posts.update(req.params.id, req.body)
+      .then((post) => {
+        if (post) {
+          res.status(200).json(post)
+        } else {
+          res.status(404).json({
+            message: "The post with the specified ID does not exist"
+          })
+        }
+      })
+      .catch((err) => {
+        console.log(err)
+        res.status(500).json({
+         message: "The posts information could not be modified"
+        }) 
+       })
+})
