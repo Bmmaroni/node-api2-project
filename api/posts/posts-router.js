eddd7dfc9e85f4413ff9a58bbc2da89b5a89070b
Brandon Maroni <63432771+Bmmaroni@users.noreply.github.com>
2021-02-13 13:36:10 -0700

    finished GET endpoint for comments

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -73,7 +73,45 @@ router.put("/api/posts/:id", (req, res) => {
       .catch((err) => {
         console.log(err)
         res.status(500).json({
-         message: "The posts information could not be modified"
+         message: "The post information could not be modified"
+        }) 
+       })
+})
+
+router.delete("/api/posts/:id", (req, res) => {
+  posts.remove(req.params.id)
+      .then((post) => {
+        if (post) {
+          res.status(200).json(post)
+        } else {
+          res.status(404).json({
+            message: "The post with the specified ID does not exist"
+          })
+        }
+      })
+      .catch((err) => {
+        console.log(err)
+        res.status(500).json({
+         message: "The post could not be removed"
+        }) 
+       })
+})
+
+router.get("/api/posts/:id/comments", (req, res) => {
+  posts.findPostComments(req.params.id)
+      .then((comments) => {
+        if (comments) {
+          res.status(200).json(comments)
+        } else {
+          res.status(404).json({
+            message: "The post with the specified ID does not exist"
+          })
+        }
+      })
+      .catch((err) => {
+        console.log(err)
+        res.status(500).json({
+         message: "The comments information could not be retrieved"
         }) 
        })
 })
